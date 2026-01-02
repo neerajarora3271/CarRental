36fc36d7d2c7335f2455e36a6b52db64ef4eef21
neerajarora3271 <theneerajarora123@gmail.com>
2026-01-02 12:33:50 +0530

    new index.js

--- Server/index.js
+++ Server/index.js
@@ -1,3 +1,39 @@
+// const express = require("express");
+// const cors = require("cors");
+// const dotenv = require("dotenv");
+// const connectDB = require("./config/db");
+
+// dotenv.config();
+// const app = express();
+
+// const userRoute = require("./routes/UserRoutes");
+// const OwnerRoute = require("./routes/ownerRoutes");
+// const bookingRoute = require("./routes/bookingRoutes");
+
+// // middleware
+// app.use(cors());
+// app.use(express.json());
+
+// connectDB();
+
+// app.get("/", (req, res) => {
+//   res.send("Server is running");
+// });
+
+// app.use("/api/user", userRoute);
+// app.use("/api/owner", OwnerRoute);
+// app.use("/api/bookings", bookingRoute);
+
+// //  ONLY listen locally
+// if (process.env.NODE_ENV !== "production") {
+//   const PORT = 7000;
+//   app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+//   });
+// }
+
+
+// module.exports = app;
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
@@ -14,8 +50,10 @@ const bookingRoute = require("./routes/bookingRoutes");
 app.use(cors());
 app.use(express.json());
 
+// DB
 connectDB();
 
+// test route
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
@@ -24,13 +62,10 @@ app.use("/api/user", userRoute);
 app.use("/api/owner", OwnerRoute);
 app.use("/api/bookings", bookingRoute);
 
-//  ONLY listen locally
-if (process.env.NODE_ENV !== "production") {
-  const PORT = 7000;
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
+const PORT = process.env.PORT || 7000;
 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 module.exports = app;
